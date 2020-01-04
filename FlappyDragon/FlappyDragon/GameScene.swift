fe623b91b8d6c155abd19eff7508cb568d9560ec
Anderson Alencar <andersonalencar@MacBook-Air-de-Anderson.local>
2020-01-04 01:10:24 -0300

    adding sounds and ending

--- FlappyDragon/FlappyDragon/GameScene.swift
+++ FlappyDragon/FlappyDragon/GameScene.swift
@@ -30,6 +30,8 @@ class GameScene: SKScene {
         invisibleFloor.physicsBody?.isDynamic = false
         invisibleFloor.position = CGPoint(x: self.size.width/2, y: self.size.height - gameArea)
         invisibleFloor.zPosition = 2
+        invisibleFloor.physicsBody?.categoryBitMask = enemyCategory
+        invisibleFloor.physicsBody?.contactTestBitMask = playerCategory
         return invisibleFloor
     }()
     lazy var invisibleRoof: SKNode = {
@@ -77,22 +79,57 @@ class GameScene: SKScene {
         scoreLabel.alpha = 0.8
         return scoreLabel
     }()
+    lazy var gameOverLabel: SKLabelNode = {
+        let gameOverLabel = SKLabelNode(fontNamed: "Chalkduster")
+        gameOverLabel.fontSize = 40
+        gameOverLabel.text = "Game Over !"
+        gameOverLabel.zPosition = 5
+        gameOverLabel.position = CGPoint(x: size.width/2+15, y: size.height/2)
+        gameOverLabel.fontColor = UIColor.red
+        return gameOverLabel
+    }()
+    lazy var restartLabel: SKLabelNode = {
+        let restartLabel = SKLabelNode(fontNamed: "Chalkduster")
+        restartLabel.fontSize = 40
+        restartLabel.text = "Restart"
+        restartLabel.zPosition = 5
+        restartLabel.position = CGPoint(x: size.width/2+15, y: size.height/2 - 50)
+        restartLabel.fontColor = UIColor.red
+        
+        let introAction = SKAction.fadeAlpha(to: 0.5, duration: 0.6)
+        let introAction2 = SKAction.fadeAlpha(to: 1, duration: 0.6)
+        let sequenceAction = SKAction.sequence([introAction, introAction2])
+        let repeatAction = SKAction.repeatForever(sequenceAction)
+        restartLabel.run(repeatAction)
+        return restartLabel
+    }()
     lazy var background: SKSpriteNode = {
            let background = SKSpriteNode(imageNamed: "background")
            background.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
            background.zPosition = 0
            return background
        }()
+    lazy var scoreSound: SKAction = {
+        let scoreSound = SKAction.playSoundFileNamed("score.mp3", waitForCompletion: false)
+        return scoreSound
+    }()
+    lazy var gameOverSound: SKAction = {
+        let gameOverSound = SKAction.playSoundFileNamed("hit.mp3", waitForCompletion: false)
+        return gameOverSound
+    }()
+    lazy var timer: Timer = {
+        return Timer()
+    }()
     lazy var velocity: Double = {
         return 100.0
     }()
     lazy var gameArea: CGFloat = {
         return 410.0
     }()
-    lazy var gameFinish: Bool = {
+    lazy var gameFinished: Bool = {
         return false
     }()
-    lazy var gameStart: Bool = {
+    lazy var gameStarted: Bool = {
         return false
     }()
     lazy var restart: Bool = {
@@ -104,9 +141,21 @@ class GameScene: SKScene {
     lazy var flyForce: CGFloat = {
         return 30.0
     }()
+    lazy var playerCategory: UInt32 = {
+        return 1
+    }()
+    lazy var enemyCategory: UInt32 = {
+        return 2
+    }()
+    lazy var scoreCategory: UInt32 = {
+        return 4
+    }()
+    
+    weak var gameViewController: GameViewController?
     
     override func didMove(to view: SKView) {
         
+        physicsWorld.contactDelegate = self
         addChild(background)
         addChild(floor)
         addChild(invisibleFloor)
@@ -128,12 +177,16 @@ class GameScene: SKScene {
         enemyTop.zPosition = 1
         enemyTop.physicsBody = SKPhysicsBody(rectangleOf: enemyTop.size)
         enemyTop.physicsBody?.isDynamic = false
+        enemyTop.physicsBody?.categoryBitMask = enemyCategory
+        enemyTop.physicsBody?.contactTestBitMask = playerCategory
         
         let enemyBottom = SKSpriteNode(imageNamed: "enemybottom\(enemynumber)")
         enemyBottom.position = CGPoint(x: size.width + enemyWidth/2, y: enemyTop.position.y - enemyTop.size.height - enemiesDistance)
         enemyBottom.zPosition = 1
         enemyBottom.physicsBody = SKPhysicsBody(rectangleOf: enemyTop.size)
         enemyBottom.physicsBody?.isDynamic = false
+        enemyBottom.physicsBody?.categoryBitMask = enemyCategory
+        enemyBottom.physicsBody?.contactTestBitMask = playerCategory
         
         let distance = size.width + enemyWidth
         let duration = Double(distance)/velocity
@@ -141,17 +194,46 @@ class GameScene: SKScene {
         let removeAction = SKAction.removeFromParent()
         let sequenceAction = SKAction.sequence([moveAction,removeAction])
         
+        let laser = SKNode()
+        laser.position = CGPoint(x: enemyTop.position.x + enemyWidth/2, y: enemyTop.position.y - enemyHeigth/2 - enemiesDistance/2)
+        laser.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 1, height: enemiesDistance))
+        laser.physicsBody?.isDynamic = false
+        laser.physicsBody?.categoryBitMask = scoreCategory
+        
         enemyTop.run(sequenceAction)
         enemyBottom.run(sequenceAction)
+        laser.run(sequenceAction)
+        
         addChild(enemyTop)
         addChild(enemyBottom)
+        addChild(laser)
 
+    }
+    
+    func gameOver() {
+        timer.invalidate()
+        player.zRotation = 0
+        player.texture = SKTexture(imageNamed: "playerDead")
+        player.physicsBody?.isDynamic = false
+        for node in self.children {
+            node.removeAllActions()
+        }
+        gameFinished = true
+        gameStarted = false
+        
+        Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { (timer) in
+            self.addChild(self.gameOverLabel)
+            self.addChild(self.restartLabel)
+            self.restart = true
+        }
+        
+        
     }
    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 
-        if !gameFinish {
-            if !gameStart {
+        if !gameFinished {
+            if !gameStarted {
                 intro.removeFromParent()
                 addChild(scoreLabel)
                 
@@ -159,15 +241,22 @@ class GameScene: SKScene {
                 player.physicsBody?.isDynamic = true
                 player.physicsBody?.allowsRotation = true
                 player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: flyForce))
+                player.physicsBody?.categoryBitMask = playerCategory
+                player.physicsBody?.contactTestBitMask = scoreCategory
+                player.physicsBody?.collisionBitMask = enemyCategory
                 
-                gameStart = true
+                gameStarted = true
                 
-                Timer.scheduledTimer(withTimeInterval: 2.5, repeats: true) { (tiemr) in
+                timer = Timer.scheduledTimer(withTimeInterval: 1.5, repeats: true) { (tiemr) in
                     self.spawnEnimeis()
                 }
             } else {
                 player.physicsBody?.velocity = CGVector.zero // zerando a velocidade para que ele não sofra a força gerada pela acelaração da gravidade e seja aplicada apenas a flyForce como resultante
                 player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: flyForce))
+            }
+        } else {
+            if restart {
+                gameViewController?.presentScene()
             }
         }
     }
@@ -176,10 +265,33 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         
-        if gameStart {
-            let yVelocity = player.physicsBody!.velocity.dy * 0.001 as CGFloat // a rotação acontece em radianos, por isso diminuimos bastante o valor da velocidade para compensar a rotação
+        if gameStarted {
+            let yVelocity = player.physicsBody!.velocity.dy  * 0.001 as CGFloat
+// a rotação acontece em radianos, por isso diminuimos bastante o valor da velocidade para compensar a rotação
             player.zRotation = yVelocity
             
         }
     }
+}
+
+
+extension GameScene: SKPhysicsContactDelegate {
+    
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        if gameStarted {
+            
+            if contact.bodyA.categoryBitMask == scoreCategory || contact.bodyB.categoryBitMask == scoreCategory {
+                score += 1
+                scoreLabel.text = "\(score)"
+                run(scoreSound)
+            } else if contact.bodyA.categoryBitMask == enemyCategory || contact.bodyB.categoryBitMask == enemyCategory{
+                run(gameOverSound)
+                gameOver()
+                
+            }
+        }
+    }
+    
+    
 }
--- FlappyDragon/FlappyDragon/GameViewController.swift
+++ FlappyDragon/FlappyDragon/GameViewController.swift
@@ -9,28 +9,46 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+import AVFoundation
 
-var stage: SKView!
 
 class GameViewController: UIViewController {
 
+    var stage: SKView!
+    lazy var musicPlayer: AVAudioPlayer = {
+        var musicPlayer = AVAudioPlayer()
+        if let musicURL = Bundle.main.url(forResource: "music", withExtension: "m4a") {
+            musicPlayer = try! AVAudioPlayer(contentsOf: musicURL)
+            musicPlayer.numberOfLoops = -1 // rodar indefinidamente
+            
+        }
+        musicPlayer.volume = 0.01
+        
+        return musicPlayer
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         stage = view as? SKView
         stage.ignoresSiblingOrder = true
-        
         presentScene()
+        musicPlayer.play()
         
     }
+    
 
     func presentScene() {
         
         let scene = GameScene(size: CGSize(width: 320, height: 568))
+        scene.gameViewController = self
         scene.scaleMode = SKSceneScaleMode.aspectFill
-        stage.presentScene(scene)
+        stage.presentScene(scene, transition: .doorsOpenVertical(withDuration: 0.5))
     }
     
+    func playMusic() {
+        
+    }
 
     override var prefersStatusBarHidden: Bool {
         return true
